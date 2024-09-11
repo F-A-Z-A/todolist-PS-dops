146f52031b8d69e9bc2f31611ea723580ec7fa66
AndreyUrazaev <f.a.z.a@bk.ru>
2024-09-11 12:12:39 +0500

    PS-lesson-03

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,88 +1,72 @@
-import React, {ChangeEvent, KeyboardEvent, useRef, useState} from 'react';
+import React, {KeyboardEvent, useRef} from 'react';
 import {FilterValuesType} from './App';
+import {useAutoAnimate} from "@formkit/auto-animate/react";
 
 type TaskType = {
-    id: string
-    title: string
-    isDone: boolean
+	id: string
+	title: string
+	isDone: boolean
 }
 
 type PropsType = {
-    title: string
-    tasks: Array<TaskType>
-    removeTask: (taskId: string) => void
-    changeFilter: (value: FilterValuesType) => void
-    addTask: (title: string) => void
+	title: string
+	tasks: Array<TaskType>
+	removeTask: (taskId: string) => void
+	changeFilter: (value: FilterValuesType) => void
+	addTask: (title: string) => void
+	children?: React.ReactNode
 }
 
 export function Todolist(props: PropsType) {
-    let [title, setTitle] = useState("")
-
-    const addTask = () => {
-        props.addTask(title);
-        setTitle("");
-    }
-
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
-    }
-
-
-    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            addTask();
-        }
-    }
-
-    const onAllClickHandler = () => props.changeFilter("all");
-    const onActiveClickHandler = () => props.changeFilter("active");
-    const onCompletedClickHandler = () => props.changeFilter("completed");
-
-    return <div>
-        <h3>{props.title}</h3>
-        <div>
-            <input value={title}
-                   onChange={onChangeHandler}
-                   onKeyPress={onKeyPressHandler}
-            />
-            <button onClick={addTask}>+</button>
-        </div>
-        <ul>
-            {
-                props.tasks.map(t => {
-
-                    const onClickHandler = () => props.removeTask(t.id)
-
-                    return <li key={t.id}>
-                        <input type="checkbox" checked={t.isDone}/>
-                        <span>{t.title}</span>
-                        <button onClick={onClickHandler}>x</button>
-                    </li>
-                })
-            }
-        </ul>
-        <div>
-            <button onClick={onAllClickHandler}>All</button>
-            <button onClick={onActiveClickHandler}>Active</button>
-            <button onClick={onCompletedClickHandler}>Completed</button>
-        </div>
-    </div>
+	const [listRef] = useAutoAnimate<HTMLUListElement>()
+	let onChangeRef = useRef<HTMLInputElement>(null)
+	
+	const addTask = () => {
+		if (onChangeRef.current) {
+			props.addTask(onChangeRef.current.value)
+			console.log(onChangeRef.current.value)
+			onChangeRef.current.value = ''
+		}
+	}
+	
+	const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			addTask();
+		}
+	}
+	
+	const onAllClickHandler = () => props.changeFilter("all");
+	const onActiveClickHandler = () => props.changeFilter("active");
+	const onCompletedClickHandler = () => props.changeFilter("completed");
+	
+	return <div>
+		<h3>{props.title}</h3>
+		<div>
+			<input ref={onChangeRef}
+			       onKeyDown={onKeyPressHandler}
+			/>
+			<button onClick={addTask}>+</button>
+		</div>
+		<ul ref={listRef}>
+			{
+				props.tasks.map(t => {
+					const onClickHandler = () => props.removeTask(t.id)
+					return <li key={t.id}>
+						<input type="checkbox" checked={t.isDone}/>
+						<span>{t.title}</span>
+						<button onClick={onClickHandler}>x</button>
+					</li>
+				})
+			}
+		</ul>
+		<div>
+			<button onClick={onAllClickHandler}>All</button>
+			<button onClick={onActiveClickHandler}>Active</button>
+			<button onClick={onCompletedClickHandler}>Completed</button>
+		</div>
+		{props.children}
+	</div>
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //----------------------------------------------------------------------------------
@@ -167,3 +151,4 @@ export function Todolist(props: PropsType) {
 //         {children}
 //     </div>
 // }
+
